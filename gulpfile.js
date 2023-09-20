821ab3bf8de0ad5dce38c249fd1e748d1b7ffc06
AlekseyGurev <gurevaleksey@yandex.ru>
2023-09-20 12:19:04 +0300

    правки

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,6 @@ import browser from 'browser-sync';
 import terser from 'gulp-terser';
 import squoosh from 'gulp-libsquoosh';
 import svgo from 'gulp-svgmin';
-import svgstore from 'gulp-svgstore';
 import del from 'del';
 import { stacksvg } from "gulp-stacksvg";
 
